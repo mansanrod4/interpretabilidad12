4a5f3f172177d92cfa1a88e979e099e274ff21a5
Juan Emilio Ordóñez Márquez <80113905+juaordmar@users.noreply.github.com>
2023-06-02 12:41:29 +0200

    Separabilidad, estabilidad y selectividad

--- metrics.py
+++ metrics.py
@@ -1,5 +1,6 @@
 import numpy
 from scipy.spatial.distance import cdist
+from itertools import combinations
 
 #Identidad
 
@@ -15,9 +16,56 @@ def identidad(xa, xb, ea, eb, metrica_distancia='euclidean'):
     
 #Separabilidad
 
+def separability(importances, true_labels):
+    classes = np.unique(true_labels)
+    num_classes = len(classes)
+
+    class_importances = [np.mean([importance for importance, label in zip(importances, true_labels) if label == c]) for c in classes]
+    class_stds = [np.std([importance for importance, label in zip(importances, true_labels) if label == c]) for c in classes]
+
+    combinations_list = list(combinations(range(num_classes), 2))
+    separabilities = []
+    
+    for class1, class2 in combinations_list:
+        separability = (class_importances[class1] - class_importances[class2]) / (class_stds[class1] + class_stds[class2])
+        separabilities.append(separability)
+
+    average_separability = np.mean(separabilities)
+
+    return average_separability
+
 #Estabilidad
 
+def stability(importances, perturbed_importances):
+    classes = range(len(importances))
+    num_classes = len(classes)
+
+    combinations_list = list(combinations(classes, 2))
+    stabilities = []
+
+    for class1, class2 in combinations_list:
+        stability = np.mean(np.abs(importances[class1] - importances[class2]) / np.abs(perturbed_importances[class1] - perturbed_importances[class2]))
+        stabilities.append(stability)
+
+    average_stability = np.mean(stabilities)
+
+    return average_stability
+
 #Selectividad
+
+def selectivity(importances):
+    num_classes = len(importances)
+    num_features = len(importances[0])
+
+    selectivities = []
+
+    for feature in range(num_features):
+        selectivity = np.mean([importances[c][feature] for c in range(num_classes)])
+        selectivities.append(selectivity)
+
+    average_selectivity = np.mean(selectivities)
+
+    return average_selectivity
 
 #Coherencia
 
