7585edf5ec6774965a40182ecefff14f58eb9a71
Manuel Sánchez Rodríguez <mansanrod4@alum.us.es>
2023-06-11 15:58:26 +0200

    LIME para una sola muestra y con distancia coseno

--- interpretabillidad12-main/ws-project/lime.py
+++ interpretabillidad12-main/ws-project/lime.py
@@ -1,26 +1,31 @@
 import numpy
 from sklearn.linear_model import Ridge
+from sklearn.metrics.pairwise import cosine_distances
 
-def algoritm_lime(N, f, x, k, min_vals, max_vals):
 
-
+def algoritm_lime(N, model, x, k, min_vals, max_vals):
     X_perturbed = []
     R = []
     W = []
-
-    for i in range(N):
+    for n in range(N):
         selected_features = numpy.random.choice(range(len(x)), size=k, replace=False)
         x_perturbed = x.copy()
-        x_perturbed[selected_features] = numpy.random.uniform(min_vals[selected_features], max_vals[selected_features])
-        w = numpy.linalg.norm(x - x_perturbed)
+        x_perturbed[selected_features] = numpy.random.uniform(
+            min_vals[selected_features], max_vals[selected_features]
+        )
+        # Calcula la distancia coseno entre x y x_perturbed
+        dist_cosine = cosine_distances(x.reshape(1, -1), x_perturbed.reshape(1, -1))
+        w = dist_cosine[0, 0]
+        # Crea el vector de representación
         r = numpy.ones(len(x))
+        # Asigna 0 a las características que se han perturbado
         r[selected_features] = 0
+
         X_perturbed.append(x_perturbed)
         R.append(r)
-        W.append(1/w)
-    
-    Y_perturbed = f(numpy.array(X_perturbed))
+        W.append(w)
+
+    Y_perturbed = model.predict(numpy.array(X_perturbed))
     G = Ridge().fit(numpy.array(R), Y_perturbed, sample_weight=W)
 
     return G.coef_, G.intercept_
-
